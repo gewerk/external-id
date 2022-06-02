b1380d9309a8c839077220b0be780fbcfe37ffcf
Dennis Morhardt <info@dennismorhardt.de>
2022-06-02 18:55:33 +0200

    🐛 Fix querying for external ID

--- src/ExternalId.php
+++ src/ExternalId.php
@@ -253,7 +253,7 @@ class ExternalId extends Plugin
                 }
 
                 // Filter by external ID
-                if (property_exists($elementQuery, 'externalId') && $elementQuery->externalId) {
+                if ($elementQuery->getBehavior('externalId') && $elementQuery->externalId) {
                     $elementQuery->subQuery->andWhere(Db::parseParam(
                         '[[elements_external_ids.externalId]]',
                         $elementQuery->externalId,
